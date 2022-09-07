d5d1ebe2c797d6d4e3d1013cccf57b46428a4e80
Rich Kaht <adamselene1776@protonmail.com>
2022-09-07 11:03:58 -0600

    Feature/Test: Add Department class

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -5,10 +5,14 @@ require './lib/department'
 RSpec.describe Department do
   let(:customer_service) {Department.new("Customer Service")}
   let(:bobbi) {Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})}
-  let(:aaron) {Employee.new{name: "Aaron Tanaka", age: "25", salary: "90000"}}
+  let(:aaron) {Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"})}
   
   it "is an instance of its class" do
     expect(customer_service).to be_a Department
+  end
+  
+  it "has a name attribute" do
+    expect(customer_service.name).to eq("Customer Service")
   end
   
   it "has an empty array of employees by default" do
@@ -27,7 +31,7 @@ RSpec.describe Department do
     expect(customer_service.expenses).to eq(0)
   end
   
-  it "#expense will add the argument to expenses" do
+  it "#expense will add the argument to expenses attribute" do
     customer_service.expense(100)
     customer_service.expense(25)
     
